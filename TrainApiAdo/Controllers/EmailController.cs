cd710fe057a5c7812f10229c083bcf864ea02cd8
kamran atayev <atayev.k@bk.ru>
2022-08-25 09:51:03 +0400

    Task=Edit(EmailPortAndPass)

--- TrainApiAdo/Controllers/EmailController.cs
+++ TrainApiAdo/Controllers/EmailController.cs
@@ -16,15 +16,15 @@ namespace TrainApiAdo.Controllers
         {
 
             MimeMessage mail = new MimeMessage();
-            mail.From.Add(MailboxAddress.Parse("atayevkamran222@gmail.com"));
+            mail.From.Add(MailboxAddress.Parse("atayev222@bk.ru"));
             mail.To.Add(MailboxAddress.Parse($"{mailArdess}"));
             mail.Subject = "Uniser LLC";
             mail.Body = new TextPart(TextFormat.Text) { Text="Deyerli musderi"+text};
 
 
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.gmail.com", 465, true);
-            smtp.Authenticate("atayevkamran222@gmail.com", "atayevkamran2002");
+            smtp.Connect("smtp.mail.ru", 465, true); //SecureSocketOptions.StartTls
+            smtp.Authenticate("atayev222@bk.ru", "bgECC83FKK5YnYgij3ec");
             smtp.Send(mail);
             smtp.Disconnect(true);
 
